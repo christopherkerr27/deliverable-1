e0cb9ed42961ce430aefca69432ec35810542211
christopherkerr27 <56808327+christopherkerr27@users.noreply.github.com>
2020-04-09 12:40:51 -0400

    Add files via upload

--- del 1/Program.cs
+++ del 1/Program.cs
@@ -2,41 +2,44 @@ using System;
 
 namespace Deliverable1
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main()
 		{
+			string travelSuggestion = "";
+			string destination = "";
 			Console.Write("What kind of trip would you like to go on, musical, tropical, or adventurous?");
 			string vacationType = Console.ReadLine();
 			Console.Write("How many are in your group?");
 			int groupSize = Convert.ToInt32(Console.ReadLine());
 			if (vacationType == "musical")
 			{
-				string destination = "New Orleans";
+				destination = "New Orleans";
 			}
 			else if (vacationType == "tropical")
 			{
-				string destination = "Beach Vacation in Mexicoo";
+				destination = "Beach Vacation in Mexicoo";
 			}
 			else
 			{
-				string destination = "Whitewater Rafting the Grand Canyon";
+				destination = "Whitewater Rafting the Grand Canyon";
 			}
 
 			if (groupSize >= 6)
 			{
-				string travelSuggestion = "Charter Flight";
+				travelSuggestion = "Charter Flight";
 			}
 			else if (groupSize <= 5 && groupSize >= 3)
 			{
-				string travelSuggestion = "Helicopter";
+				travelSuggestion = "Helicopter";
 			}
 			else
 			{
-				string travelSuggestion = "First Class";
+				travelSuggestion = "First Class";
 			}
 
-			string result = $"Since you’re a group of {groupSize} going on a musical vacation, you should take a {travelSuggestion} to {destination}.";
+			string result = "Since you're a group of " + groupSize + " going on a " + vacationType + " you should take a " + travelSuggestion + " to " + destination;
+			Console.Write(result);
 		}
 	}
 }
